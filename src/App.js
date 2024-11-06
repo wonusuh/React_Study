a13923d9eb3a64972d08ac34dcc4602010fd30c4
Wonu Suh <wonwooseo23@gmail.com>
2024-11-06 10:41:08 +0900

    created : React study has been started.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+
+/** 헤더 영역입니다. */
+function Header() {
+  return (
+    < header >
+      <h1><a href="/">WEB</a></h1>
+    </header >
+  );
+}
+
+/** 전체 목록 영역입니다. */
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href='/read/1'>html</a></li>
+        <li><a href='/read/2'>css</a></li>
+        <li><a href='/read/3'>javascript</a></li>
+      </ol>
+    </nav>
+  );
+}
+
+/** 본문을 표시하기 위한 아티클 영역입니다. */
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /** 가장 바깥에 div가 있지 않으면 eslint 에러가 발생할 수 있습니다. */
+    <div>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
